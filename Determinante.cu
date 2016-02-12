24b120f25726490fc3301ccf3266eb9bfeccc031
Carolina <krito_j_g@utp.edu.co>
2016-02-11 22:11:00 -0500

    todavía no funciona, adelanto código gpu

--- Determinante.cu
+++ Determinante.cu
@@ -4,11 +4,48 @@
 
 using namespace std;
 
+__global__
+void Det1_CU(int *M, int filas, int columnas, int &suma){
+
+	//int i = blockIdx.y*blockDim.y+threadIdx.y;//filas
+	int j = blockIdx.x*blockDim.x+threadIdx.x;//columnas
+
+	if(j < columnas){
+		int k = j, aux = columnas, l = 0, mult = 1;
+		while(aux--){
+			if(k == columnas) k = 0;
+			mult *= M[(l*columnas)+k];
+			__syncthreads();//espera a que todos los hilos hagan la misma multiplicacion
+			k++; l++;
+		}
+		suma += mult;
+	}
+}
+
+__global__
+void Det2_CU(int *M, int filas, int columnas, int &suma){
+
+	//int i = blockIdx.y*blockDim.y+threadIdx.y;//filas
+	int j = blockIdx.x*blockDim.x+threadIdx.x;//columnas
+
+	if(j < columnas){
+
+		int k = j, aux = columnas, l = filas - 1, mult = 1;
+		while(aux--){
+			if(k == columnas) k = 0;
+			mult *= M[(l*columnas)+k];
+			__syncthreads();//espera a que todos los hilos hagan la misma multiplicacion
+			k++; l--;
+		}
+		suma += mult;
+	}
+}
+
 __host__
 int Det1(int *M, int filas, int columnas){
 	int suma = 0;
 	for(int j = 0; j < columnas; j++){
-		int k = j, aux = columnas, l = 0, mult = 1; 
+		int k = j, aux = columnas, l = 0, mult = 1;
 		while(aux--){
 			if(k == columnas) k = 0;
 			mult *= M[(l*columnas)+k];
@@ -23,7 +60,7 @@ __host__
 int Det2(int *M, int filas, int columnas){
 	int suma = 0;
 	for(int j = 0; j < columnas; j++){
-		int k = j, aux = columnas, l = filas -1, mult = 1; 
+		int k = j, aux = columnas, l = filas - 1, mult = 1;
 		while(aux--){
 			if(k == columnas) k = 0;
 			mult *= M[(l*columnas)+k];
@@ -47,7 +84,7 @@ void imprime(int* A,int filas, int columnas){//imprime como si fuera una matriz
 __host__
 void inicializa(int *A,int filas, int columnas){//inicializa arreglos
 	/* initialize random seed: */
-	srand (time(NULL));	
+	srand (time(NULL));
 	int valor;
 	for(int i=0;i<filas*columnas;i++){
 		/* generate secret number between 1 and 10: */
@@ -70,25 +107,76 @@ void llena(int *A, int filas, int columnas){
 int main(void){
 
 	cudaError_t error = cudaSuccess;//Para controlar errores
-  	int *matriz; 
-  	int filas = 3, columnas = 3, det;
+	int *d_matriz, *matriz;
+	int filas = 3, columnas = 3, h_det, d_det;
 
-  	int size = filas*columnas*sizeof(int);
+	int size = filas*columnas*sizeof(int);
 
-  	//----------------------CPU-------------------------
-  	clock_t t = clock();//Iniciamos la cuenta de reloj
-  	
-  	//Separamos memoria para el host
-  	matriz = (int*)malloc(size);
-	
+	//----------------------CPU-------------------------
+
+	//Separamos memoria para el host
+	matriz = (int*)malloc(size);
+
 	//Inicializamos los valores de la matriz
 	//llena(matriz, filas, columnas);
 	inicializa(matriz, filas, columnas);
-	
-	imprime(matriz, filas, columnas);	
+
+	clock_t t = clock();//Iniciamos la cuenta de reloj
+
+	//imprime(matriz, filas, columnas);
 	//Calculamos el determinante
-	det = Det1(matriz, filas, columnas) - Det2(matriz, filas, columnas);
-	cout << endl << "Determinante = " << det << endl;
-	
+	h_det = Det1(matriz, filas, columnas) - Det2(matriz, filas, columnas);
+	//cout << endl << "Determinante = " << h_det << endl;
+
+	t = clock() - t;//Terminamos la cuenta de reloj
+
+	double time_CPU = ((double)t) / CLOCKS_PER_SEC;
+	cout<<"El tiempo transcurrido en la CPU fue: "<<time_CPU<<endl;
+
+	//---------------------GPU------------------------------
+	t = clock();//Iniciamos la cuenta de reloj
+
+  //Separamos memoria para el device
+  error = cudaMalloc((void**)&d_matriz,size);
+  if(error != cudaSuccess){
+    cout << "Error reservando memoria para d_matriz" << endl;
+    //return -1;
+  }
+
+	//Copiamos datos del host al device
+  error = cudaMemcpy(d_matriz,matriz,size,cudaMemcpyHostToDevice);//destino d_matriz y origen matriz
+  if(error != cudaSuccess){
+      cout << "Error copiando los datos de matriz a d_matriz" << endl;
+      //exit(-1);
+  }
+
+	//Lanzamos el kernel
+
+  dim3 dimblock(3,1,1);//solo necesitamos los hilos de las columnas
+  dim3 dimGrid(1,1,1);
+  //dim3 dimGrid(ceil((double)(columnas/32)),ceil((double)(filas/32)),1);
+
+	int ans1 = 0, ans2 = 0;
+	//Det1_CU<<<dimGrid,dimblock>>>(d_matriz, filas, columnas, ans1);
+	//Det2_CU<<<dimGrid,dimblock>>>(d_matriz, filas, columnas, ans2);
+
+	cudaDeviceSynchronize();
+
+	d_det = ans1 - ans2;
+
+  t = clock() - t;//Terminamos la cuenta de reloj
+
+  double time_GPU = ((double)t) / CLOCKS_PER_SEC;
+	cout<<"El tiempo transcurrido en la GPU fue: "<<time_GPU<<endl;
+  //------------------------------------------------------------
+  cout<<"El tiempo de aceleramiento fue: "<<time_CPU/time_GPU<<endl;
+
+	if(h_det == d_det) cout << "Buen cálculo" << endl;
+	else cout << "Mal cálculo" << endl;
+
+	//Liberamos memoria
+	free(matriz);
+	cudaFree(d_matriz);
+
 	return 0;
 }
