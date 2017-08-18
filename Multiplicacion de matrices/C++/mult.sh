37acf13b157133b48dc2c2c478af23bf08902c4d
Carolina <krito_j_g@utp.edu.co>
2017-08-17 19:19:43 -0500

    Agrega archivos para ejecutar con ayuda de slurm

---
+++ Multiplicacion de matrices/C++/mult.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#!/bin/bash
+#SBATCH --job-name=mult
+#SBATCH --output=mult.out
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=10:00
+#SBATCH --mem-per-cpu=100
+
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+
+./mult ../C/in1.in ../C/in2.in
