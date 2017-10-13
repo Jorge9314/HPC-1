dc7e03d7bf69c58366a605139ed8dbc8fab2ff3f
Carolina <krito_j_g@utp.edu.co>
2017-10-13 09:09:13 -0500

    Modifica script en bash para correr el algoritmo de Sobel en memoria gloabl 20 veces por cada imagen

--- Procesamiento de imágenes/Sobel/Sobel.sh
+++ Procesamiento de imágenes/Sobel/Sobel.sh
@@ -6,10 +6,20 @@
 #SBATCH --nodes=1
 #SBATCH  --gres=gpu:1
 
-
 export PATH=/usr/local/cuda-8.0/bin${PATH:+:${PATH}}
 export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64/${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
 
 export CUDA_VISIBLE_DEVICES=1
 
-./build/a.out ../images/cats.jpg
+imageFolder="../../images"
+
+i=0
+for image in $imageFolder/*
+do
+  # echo "image : ${image}"
+  for i in `seq 1 20`;
+  do
+    ./global.out ${image}
+    # echo $i
+  done
+done
