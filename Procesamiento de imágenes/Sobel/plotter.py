dc016918916da0f4a39256ec421f857efa0ae81f
Carolina <krito_j_g@utp.edu.co>
2017-10-14 15:17:09 -0500

    Modifica plotter para que sea utilizado de forma más dinámica

--- Procesamiento de imágenes/Sobel/plotter.py
+++ Procesamiento de imágenes/Sobel/plotter.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import sys
 
 def readData(fileName):
+    N = 0
     f = open(fileName,"r")
     data = f.readlines()
 
@@ -11,18 +12,22 @@ def readData(fileName):
         words = data[i].split()
         # print (words)
         key = words[1]
+        if (i == 1):
+            key_aux = words[1]
+        if (key_aux == words[1] or i == 1):
+            N += 1
         if (key in dic):
             dic[key] = [float(words[0]), dic[key][1] + float(words[2])]
         else:
             dic[key] = [float(words[0]), float(words[2])]
 
-    return dic
+    return dic, N
 
-def getAxis(x, y, dic):
+def getAxis(x, y, dic, N):
     for e in dic: # get the average of the times
-        dic[e][1] = round(dic[e][1]/20, 3)
-        # print (e + " -> ")
-        # print (dic[e])
+        dic[e][1] = round(dic[e][1]/N, 3)
+        print (e + " -> ")
+        print (dic[e])
         x_aux.append(dic[e][1])
         y_aux.append(dic[e][0])
 
@@ -34,18 +39,21 @@ if __name__ == "__main__":
         print ("Usage: <times fileName> <plot title>")
         sys.exit()
 
-    dic = readData(sys.argv[1])
+    dic, N = readData(sys.argv[1])
+    print ("N: ", N)
     x_aux = []
     y_aux = []
-    getAxis(x_aux, y_aux, dic)
+    getAxis(x_aux, y_aux, dic, N)
 
     x = np.array(x_aux)
     y = np.array(y_aux)
+    plt.subplot(111)
     plt.xlabel('time (seconds)')
-    plt.ylabel('size (bytes)')
+    plt.ylabel('image size (height*width)')
     plt.scatter(x, y)
-    # plt.plot(x, y)
-    plt.yscale('linear')
+    # plt.yscale('linear')
     plt.title('Sobel Filter, ' + sys.argv[2])
     plt.grid(True)
-    plt.show()
+    # plt.plot(x, y)
+    # plt.show()
+    plt.savefig(sys.argv[2] + '.png')
